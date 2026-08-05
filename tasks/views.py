ba1b66f61d7df09d4dd516c5bb75e5bc8d1f7202
Nazar <hehelele281@gmail.com>
2026-08-05 14:56:11 +0300

    Implement user authentication

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
@@ -7,39 +9,40 @@ from tasks.forms import TaskForm
 from tasks.models import Task
 
 
-class TaskListView(generic.ListView):
+class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     context_object_name = "tasks"
     template_name = "tasks/task_list.html"
     paginate_by = 5
 
 
-class TaskDetailView(generic.DetailView):
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
     model = Task
     template_name = "tasks/task_detail.html"
     context_object_name = "task"
 
 
-class TaskCreateView(generic.CreateView):
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
     model = Task
     form_class = TaskForm
     template_name = "tasks/task_form.html"
     success_url = reverse_lazy("tasks:task-list")
 
 
-class TaskUpdateView(generic.UpdateView):
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Task
     form_class = TaskForm
     template_name = "tasks/task_form.html"
 
 
-class TaskDeleteView(generic.DeleteView):
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Task
     template_name = "tasks/task_confirm_delete.html"
     success_url = reverse_lazy("tasks:task-list")
 
 
 @require_POST
+@login_required
 def toggle_task_status(request, pk):
     task = get_object_or_404(Task, pk=pk)
 
